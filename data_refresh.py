159e393726f00c12208173b0cbe518808a9c861b
Rahul Bangre <rahul_bangre@ch.honda.com>
2023-06-09 11:21:06 -0400

    fixed bugs & added example usage

--- data_refresh.py
+++ data_refresh.py
@@ -3,27 +3,32 @@ import pandas as pd
 import hashlib
 
 '''
-Computes how similar 2 given datasets are. Consumes 3 command line arguments (datapath1, datapath2, minimum difference threshold)
+Computes if two datasets are the same, consumes two command line arguments which are the paths to the datasets. 
 
+Example 1: 
+
+command-line input: py data_refresh.py ./example-data/dataset1.csv ./example-data/dataset2.csv 
+ouput: Duplicate Datasets
+
+Example 2:
+
+command-line input: py data_refresh.py ./example-data/dataset1.csv ./example-data/dataset2.csv 
+output: Not Duplicate Datasets
 '''
-
-MINIMUM_THRESHOLD = sys.argv[3] 
 
 def hash(df):
     data_bytes = df.to_string()
+    encoded_data = data_bytes.encode('utf-8')
     hasher = hashlib.sha256()
-    hasher.update(data_bytes)
+    hasher.update(encoded_data)
     return hasher.hexdigest()
 
 def check_duplicate(ds1, ds2): 
     ds1_hash = hash(ds1)
     ds2_hash = hash(ds2)
 
-    if ds1_hash == ds2_hash: return 0
-    return 1
-
-def check_similarity(ds1, ds2):
-    pass
+    if ds1_hash == ds2_hash: return 1
+    return 0
 
 def main(dp_1, dp_2):
     dataset1 = pd.read_csv(dp_1)
@@ -32,14 +37,10 @@ def main(dp_1, dp_2):
     dataset1.columns = dataset1.columns.str.strip()
     dataset2.columns = dataset2.columns.str.strip()
 
-    if not check_duplicate(dataset1, dataset2): 
-        print('Duplicate Datasets Found')
-        return
+    isDuplicate = check_duplicate(dataset1, dataset2)
 
-    check_similarity(dataset1, dataset2)
-
-
-    
+    if isDuplicate: print('Duplicate Datasets')
+    else: print('Not Duplicate Datasets')
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
